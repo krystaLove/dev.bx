7f455da07d5d47cd28bd36085468ccab2be5cb27
Zhuravkov Denis <denis.zhuravkov@gmail.com>
2021-11-29 01:29:19 +0200

    Fixed add-movie and favorite pages

--- favorite.php
+++ favorite.php
@@ -1,12 +1,15 @@
 <?php
 
-/** @var array $genres */
-require_once "./data/movies-db.php";
 require_once "./lib/template-functions.php";
 require_once "./lib/helper-functions.php";
+require_once "./lib/movies-functions.php";
+require_once "./lib/db-functions.php";
 
 /** @var array $config */
 require_once "./config/app.php";
+
+$moviesDb = createDbConnection($config['db']);
+$genres = getGenres($moviesDb);
 
 $page = renderTemplate("./resources/pages/favorite.php");
 
