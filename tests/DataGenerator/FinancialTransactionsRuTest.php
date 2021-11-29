519bf0e5c868dbf2d29f3ef9bf5b1e60c27c4774
Zhuravkov Denis <denis.zhuravkov@gmail.com>
2021-11-29 02:42:46 +0200

    Added some tests

--- tests/DataGenerator/FinancialTransactionsRuTest.php
+++ tests/DataGenerator/FinancialTransactionsRuTest.php
@@ -19,6 +19,24 @@ class FinancialTransactionsRuTest extends TestCase
 					'BIC' => '',
 					'CorrespAcc' => ''
 				]
+			],
+			'filled but mistyped' => [
+				[
+					'Name' => [32131],
+					'PersonalAcc' => 'asdad',
+					'BankName' => 'asdasd',
+					'BIC' => 'asdsad',
+					'CorrespAcc' => 'adasdsad'
+				]
+			],
+			'filled but length is long' => [
+				[
+					'Name' => str_repeat('a', 161),
+					'PersonalAcc' => str_repeat('a', 21),
+					'BankName' => str_repeat('a', 46),
+					'BIC' => str_repeat('a', 9),
+					'CorrespAcc' => str_repeat('a', 21)
+				]
 			]
 		];
 	}
@@ -50,6 +68,42 @@ class FinancialTransactionsRuTest extends TestCase
 
 		$result = $dataGenerator->validate();
 		static::assertTrue($result->isSuccess());
+	}
+
+	public function testGetData() : FinancialTransactionsRu
+	{
+		$dataGenerator = new FinancialTransactionsRu();
+		$dataGenerator
+			->setName('Name')
+			->setBIC('BIC')
+			->setBankName('BankName')
+			->setCorrespondentAccount('CorrespondentAccount')
+			->setPersonalAccount('PersonalAcc');
+
+		$toCheck = "ST00012|Name=Name|PersonalAcc=PersonalAcc|BankName=BankName|BIC=BIC|CorrespAcc=CorrespondentAccount";
+		static::assertEquals($toCheck, $dataGenerator->getData());
+
+		return $dataGenerator;
+	}
+
+	/**
+	 * @depends testGetData
+	 */
+	public function testAddFields($dataGenerator) : void
+	{
+		$dataGenerator->addFields(['test'=> 1, 'test2' => 2]);
+
+		static::assertStringContainsString("test=1|test2=2", $dataGenerator->getData());
+	}
+
+	/**
+	 * @depends testGetData
+	 */
+	public function testSetField($dataGenerator) : void
+	{
+		$dataGenerator->setField('test3', 3);
+
+		static::assertStringContainsString("test3=3", $dataGenerator->getData());
 	}
 
 }
