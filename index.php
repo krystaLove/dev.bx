a8d636942ff105e50fcdf2dafc9cc6d1d0185ea4
Zhuravkov Denis <denis.zhuravkov@gmail.com>
2021-12-15 03:20:03 +0200

    Client code with subscribing logger on onPurchasedGold event

--- index.php
+++ index.php
@@ -1,37 +1,11 @@
 <?php
 
-use Entity\Service;
-use Entity\User;
-
 spl_autoload_register(function ($class) {
 	include __DIR__ . '/' . str_replace("\\", "/",  $class) . '.php';
 });
 
-//\Event\EventBus::getInstance()->subscribe("onUserAdd", "\\Helper\\Subscriber::onUserAdd");
-//\Event\EventBus::getInstance()->subscribe("onUserUpdate", "\\Helper\\Subscriber::onUserUpdate");
-//
-//
-//$user = new User();
-//$user
-//	->setId(1)
-//	->setName('Ivan')
-//	->add()
-//	->update()
-//;
+\Event\EventBus::getInstance()->subscribe("onPurchasedGold", "\Helper\Logger::addMessageToLog");
 
-//$service = new Service();
-//$context = new \State\ServiceStateContext(new \State\ActiveState($service));
-//
-//var_dump($context);
-//var_dump($context->changeState());
-//var_dump($context->changeState());
+\Strategy\PurchaseServiceContext::purchase(new \Strategy\PurchasePremiumGoldStrategy());
 
-//function purchasePremiumLite()
-//{
-	var_dump(\Strategy\PurchaseServiceContext::purchase(new \Strategy\PurchasePremiumLiteStrategy()));
-//}
-
-//function purchasePremium($type)
-//{
-	var_dump(\Strategy\PurchaseServiceContext::purchase(new \Strategy\PurchasePremiumStrategy()));
-//}
+\Helper\Logger::writeToFile();
